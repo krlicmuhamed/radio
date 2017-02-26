bf812bca602936b0e8e2cdce651895f92ddce44c
Muhamed Krlić <1l0oky1@gmail.com>
2017-02-26 01:08:15 +0100

    Send and receive.

--- src/main.ino
+++ src/main.ino
@@ -1,16 +1,43 @@
 #include <VirtualWire.h>
 
+#define TX_RADIO 2
+#define RX_RADIO 4
+
+unsigned long previousMillis = 0;
+byte message[VW_MAX_MESSAGE_LEN]; // a buffer to store the incoming messages
+byte messageLength = VW_MAX_MESSAGE_LEN; // the size of the message
+
 void setup() {
-    // Initialize the IO and ISR
-    vw_setup(2000); // Bits per sec
+  Serial.begin(9600);
+  Serial.println("Device is ready");
+  // Initialize the IO and ISR
+  vw_setup(2000); // Bits per sec
+  vw_set_tx_pin((uint8_t) TX_RADIO);
+  vw_set_rx_pin((uint8_t) RX_RADIO);
+  vw_rx_start(); // Start the receiver
 }
 
+// Non-blocking, ftw!
 void loop() {
-    send("Hello there");
-    delay(1000);
+  unsigned long currentMillis = millis();
+
+  if (vw_get_message(message, &messageLength)) {
+    Serial.print("Received: ");
+
+    for (int i = 0; i < messageLength; i++) {
+      Serial.write(message[i]);
+    }
+
+    Serial.println();
+  }
+
+  if(currentMillis - previousMillis > 1000) {
+    previousMillis = currentMillis;
+    send((char *)"Hello there");
+  }
 }
 
 void send (char *message) {
-    vw_send((uint8_t *)message, strlen(message));
-    vw_wait_tx(); // Wait until the whole message is gone
+  vw_send((uint8_t *)message, strlen(message));
+  //vw_wait_tx(); // Wait until the whole message is gone
 }
